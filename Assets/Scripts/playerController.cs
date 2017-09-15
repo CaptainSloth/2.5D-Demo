9ff5b7fc26b5dfc2376bacd75c4fd178468aedc4
robawesome <robduboff@gmail.com>
2017-09-15 15:14:45 -0700

    Added gunFX

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -11,6 +11,14 @@ public class playerController : MonoBehaviour {
 
     bool facingRight;
 
+    //for jumping
+    bool grounded = false;
+    Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
@@ -25,16 +33,31 @@ public class playerController : MonoBehaviour {
 
     void FixedUpdate()
     {
+        if(grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            animator.SetBool("grounded", grounded);
+            rb.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+        //Ground Check
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (groundCollisions.Length > 0) grounded = true;
+        else grounded = false;
+
+        animator.SetBool("grounded", grounded);
+
         float move = Input.GetAxis("Horizontal");
         animator.SetFloat("speed", Mathf.Abs(move));
 
         float sneak = Input.GetAxisRaw("Fire3");
         animator.SetFloat("sneak", sneak);
 
+        float firing = Input.GetAxisRaw("Fire1");
+        animator.SetFloat("shooting", firing);
 
-        if (sneak > 0)
+        if ((sneak > 0 || firing>0) && grounded)
         {
-            rb.velocity = new Vector3(move * walkSpeed, rb.velocity.y, 0f);
+           rb.velocity = new Vector3(move * walkSpeed, rb.velocity.y, 0f);
         }
         else
         {
@@ -53,5 +76,11 @@ public class playerController : MonoBehaviour {
         Vector3 scale = transform.localScale;
         scale.z *= -1;
         transform.localScale = scale;
+    }
+
+    public float GetFacing()
+    {
+        if (facingRight) return 1;
+        else return -1;
     }
 }
---
+++ Assets/fireBullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class fireBullet : MonoBehaviour {
+
+    public float timeBetweenBullets = 0.15f;
+    public GameObject projectile;
+
+    float nextBullet;
+
+	// Use this for initialization
+	void Awake () {
+        nextBullet = 0f;
+        Debug.Log("Pew");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerController player = transform.root.GetComponent<playerController>();
+
+        if (Input.GetAxisRaw("Fire1") > 0 && nextBullet < Time.time)
+        {
+            nextBullet = Time.time + timeBetweenBullets;
+            Vector3 rot;
+            if (player.GetFacing() == -1f)
+            {
+                rot = new Vector3(0, -90, 0);
+            }
+            else rot = new Vector3(0, 90, 0);
+
+            Instantiate(projectile, transform.position, Quaternion.Euler(rot));
+        }
+        
+	}
+}
